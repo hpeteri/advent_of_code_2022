76fab23d46d844fcfc6f0892e7dda5bdbd494412
Henrik Peteri <P.Henrik.Rikhard@hotmail.com>
2022-12-22 23:39:47 +0200

    day 19

--- day_19/main.py
+++ day_19/main.py
@@ -2,7 +2,9 @@
 # All rights reserved.
 
 import sys
+
 from copy import copy
+from math import ceil
 
 # set working directory
 import os
@@ -23,236 +25,196 @@ with open(sys.argv[1], "r") as f:
 for idx, line in enumerate(lines):
     lines[idx] = line.strip()
 
-
-class Blueprint:
-    def __init__(this):
-        this.ore_cost      = 0
-        this.clay_cost     = 0
-        this.obsidian_cost = []
-        this.geode_cost    = []
-
-    def __str__(this):
-        return f"\nore: {this.ore_cost}\nclay: {this.clay_cost}\nobsidian: {this.obsidian_cost}\ngeode: {this.geode_cost}"
-
-
 class SimulationContext:
-    def __init__(this, blueprint):
-        this.ore_bot_count                  = 1
-        this.clay_bot_count                 = 0
-        this.obsidian_bot_count             = 0
-        this.geode_bot_count                = 0
-        this.constructed_ore_bot_count      = 0
-        this.constructed_clay_bot_count     = 0
-        this.constructed_obsidian_bot_count = 0
-        this.constructed_geode_bot_count    = 0
-        this.ore_count                      = 0
-        this.clay_count                     = 0
-        this.obsidian_count                 = 0
-        this.geode_count                    = 0
-        this.rnd                            = 0
-        this.max_ore_bot_count              = max(blueprint.ore_cost, max(blueprint.clay_cost, max(blueprint.obsidian_cost[0] ,blueprint.geode_cost[0])))
-        this.max_clay_bot_count             = blueprint.obsidian_cost[1]
-        this.max_obsidian_bot_count         = blueprint.geode_cost[1]
-        #this.history                        = ""
+    def __init__(this, costs):
+        this.resources  = [0,0,0,0]
+        this.robots     = [1,0,0,0]
+        this.minute     = 0
+        #this.target     = 24
+        this.target     = 32
+        this.costs      = costs
+        this.max_robots = [0,0,0,this.target]
+        this.history    = "\n!!! HISTORY !!! \n"
+
+        for robot_cost in costs:
+            for ci, cost in enumerate(robot_cost):
+                this.max_robots[ci] = max(this.max_robots[ci], cost)
                 
-    def print_history(this):
-        pass
-        #print("HISTORY")
-        #print(this.history)
+    def create_copy(this):
+        cp =  SimulationContext(this.costs)
+        cp.history   = this.history[:]
+        cp.resources = this.resources[:]
+        cp.robots    = this.robots[:]
+        cp.minute    = this.minute
+        return cp
         
-    def update(this):
-        this.ore_count      += this.ore_bot_count
-        this.clay_count     += this.clay_bot_count
-        this.obsidian_count += this.obsidian_bot_count
-        this.geode_count    += this.geode_bot_count
-        #this.history        += this.get_status()
-  
+    def time_for_robot(this, robot_idx):
+        costs = this.costs[robot_idx]
+        for ci, cost in enumerate(costs):
+            if not cost:
+                continue
+            
+            if this.robots[ci] == 0:
+                return -1
 
-    def next_round(this):
-        this.rnd += 1
+        if this.robots[robot_idx] == this.max_robots[robot_idx]:
+            return -1
         
-        this.ore_bot_count      += this.constructed_ore_bot_count;
-        this.clay_bot_count     += this.constructed_clay_bot_count;
-        this.obsidian_bot_count += this.constructed_obsidian_bot_count;
-        this.geode_bot_count    += this.constructed_geode_bot_count;
+        max_time = 0
+        for ci, cost in enumerate(costs):
+            if cost:
+                if cost <= this.resources[ci]:
+                    pass
+                else:
+                    max_time = max(max_time, ceil((cost - this.resources[ci]) / (this.robots[ci])))
 
-        this.constructed_ore_bot_count      = 0;        
-        this.constructed_clay_bot_count     = 0;       
-        this.constructed_obsidian_bot_count = 0;   
-        this.constructed_geode_bot_count    = 0;
-        
-        #this.history += f"\n=== minute {this.rnd} ===\n"
-        
-    def get_status(this):
-        
-        s  = f"{this.ore_bot_count} / {this.max_ore_bot_count} ore robots collects ore. you now have {this.ore_count} ore.\n"
-        s += f"{this.clay_bot_count} / {this.max_clay_bot_count} clay robots collects clay. you now have {this.clay_count} clay.\n"
-        s += f"{this.obsidian_bot_count} / {this.max_obsidian_bot_count}  obsidian robots collects obsidian. you now have {this.obsidian_count} obsidian.\n"
-        s += f"{this.geode_bot_count} geode robots collects geodes. you now have {this.geode_count} geodes.\n"
+        return max_time
 
-        return s
+    def make_robot(this, robot_idx):        
+        for ci, cost in enumerate(this.costs[robot_idx]):
+            this.resources[ci] -= cost
+                
+            this.history += "make {} robot for cost of {}\n".format(["ore", "clay", "obsidian", "geode"][robot_idx], sim.costs[robot_idx])
         
-    def make_ore_robot(this, blueprint):
-        if this.ore_count < blueprint.ore_cost:
-            return 0
+    def simulate_for(this, minutes):
+        result = True
+        
+        if this.minute + minutes > this.target - 1:
+            minutes     = this.target - 1 - this.minute
+            result = False
+        
+        this.minute += minutes
 
-        if this.ore_bot_count >= this.max_ore_bot_count:
-            return 2
         
-        this.ore_count     -= blueprint.ore_cost
-        this.constructed_ore_bot_count = 1
-        #this.history += "make ore bot\n"
-        return 1
+        for idx, robot in enumerate(this.robots):
+            this.resources[idx] += robot * minutes
+
+        return result
     
-    def make_clay_robot(this, blueprint):
-        if this.ore_count < blueprint.clay_cost:
-            return 0
-
-        if this.clay_bot_count >= this.max_clay_bot_count:
-            return 2
+    def history_start_turn(this):
+        this.history += f"=== {this.minute} ===\n"
         
-        this.ore_count -= blueprint.clay_cost
-        this.constructed_clay_bot_count = 1
-        #this.history += "make clay bot\n"
-        return 1
-
-    def make_obsidian_robot(this, blueprint):
-        if this.ore_count < blueprint.obsidian_cost[0]:
-            return 0
-
-        if this.clay_count < blueprint.obsidian_cost[1]:
-            return 0
-
-        if this.obsidian_bot_count >= this.max_obsidian_bot_count:
-            return 2
+    def history_resources(this):
+        s = f"robots:    {this.robots}\n"
+        s += f"resources: {this.resources}\n"
+        s += "----------------------------\n"
+        this.history += s
         
-        this.ore_count -= blueprint.obsidian_cost[0]
-        this.clay_count -= blueprint.obsidian_cost[1]
-        this.constructed_obsidian_bot_count = 1
-        #this.history += "make obsidian bot\n"
-        return 1
+def factorial(x):
+    result = 0
+    while x >= 0:
+        result += x
+        x -= 1
 
+    return result
     
-    def make_geode_robot(this, blueprint):
-        if this.ore_count < blueprint.geode_cost[0]:
-            return 0
-        if this.obsidian_count < blueprint.geode_cost[1]:
-            return 0
+def simulate(sim, simulations_to_do, best_result):
+    if sim.minute == sim.target - 1:
+        for i, robot in enumerate(sim.robots):
+            sim.resources[i] += robot
+        sim.history_start_turn()
+        sim.history_resources()
 
-        this.ore_count      -= blueprint.geode_cost[0]
-        this.obsidian_count -= blueprint.geode_cost[1]
-        this.constructed_geode_bot_count = 1
-        #this.history += "make geode bot\n"
-        return 1
-        
-max_geodes_opened = 0
-def sim_blueprint(blueprint, sim, simulations_to_do):
-    target_rnd = 24
-    while sim.rnd < target_rnd:
-        sim.next_round()
-        
-        #ore
-        sim_0 = copy(sim)
-        a = sim_0.make_ore_robot(blueprint)
-        if a == 1:
-            sim_0.update()
+        return
+    if sim.minute > sim.target - 1:
+        print("this should not happen")
+        print(sim.minute)
+        return
+
+    for i in range(4):
+        time_for_n_robot = sim.time_for_robot(i)
+        if time_for_n_robot >= 0:
+            sim_0 = sim.create_copy()
+            
+            sim_0.history_start_turn()
+            sim_0.history_resources()
+            
+            if time_for_n_robot == 0:
+                sim_0.make_robot(i)
+                sim_0.history_resources()
+                
+                sim_0.simulate_for(1)
+                sim_0.robots[i] += 1
+                
+            else:
+                sim_0.simulate_for(time_for_n_robot)
+                
+            if best_result:
+                time_left = sim_0.target - sim_0.minute
+
+                g0 = sim_0.robots[3] * time_left
+                g1 = factorial(time_left)
+
+                if sim_0.resources[3] + g0 + g1 <= best_result.resources[3]:
+                    return
+                                
             simulations_to_do.append(sim_0)
-
-        #clay
-        sim_1 = copy(sim)
-        b = sim_1.make_clay_robot(blueprint)
-        if b == 1:
-            sim_1.update()
-            simulations_to_do.append(sim_1)
-
-        #obisian
-        sim_2 = copy(sim)
-        c = sim_2.make_obsidian_robot(blueprint)
-        if c == 1:
-            sim_2.update()
-            simulations_to_do.append(sim_2)
-
-        #geode
-        sim_3 = copy(sim)
-        d = sim_3.make_geode_robot(blueprint)
-        if d == 1:
-            sim_3.update()
-            simulations_to_do.append(sim_3)
-
-        sim.update()
-
-        # if we can do all options, no point of waiting a round...
-        if (a == 1) and (b == 1) and (c == 1) and (d == 1):
-            break
-
-        # case when we dont construct new robots and instead wait for resources
+            
         
-        if b and (sim.clay_bot_count == 0):
-            break
-
-        if c and (sim.obsidian_bot_count == 0):
-            break
-        
-        if d and (sim.geode_bot_count == 0):
-            break
-        
-    if not sim.rnd < target_rnd:
-        global max_geodes_opened
-        if sim.geode_count > max_geodes_opened.geode_count:
-            max_geodes_opened = sim
-            print(sim.geode_count)
-                
 blueprint = None
 line_idx = 0
-geode_count = []
-for line in lines:
+blueprint_results = []
+for line in lines:    
     if line[0: 9] == "Blueprint" or len(line) == 0:
         if blueprint != None:
-            print()
             print("do blueprint: ", blueprint)
             simulations_to_do = [SimulationContext(blueprint)]
             tries = 0
-            max_geodes_opened = SimulationContext(blueprint)
+
+            best_score = 0
+            blueprint_results.append(None)
             while len(simulations_to_do):
+                
                 tries += 1
                 sim = simulations_to_do.pop()
-                sim_blueprint(blueprint, sim, simulations_to_do)
+                simulate(sim, simulations_to_do, blueprint_results[-1])
+                if sim.resources[3] > best_score:
+                    best_score = sim.resources[3]
+                    blueprint_results[-1] = sim
+                    print(f"new best: {best_score} (with {tries} tries)")
 
             print(f"did {tries} simulations")
-            max_geodes_opened.print_history()
-            geode_count.append(max_geodes_opened)
-                            
-        blueprint = Blueprint()
-        line_idx = 0
+            
+        blueprint = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        line_idx  = 0
+
+        if len(blueprint_results) == 3:
+            break
+        
         continue
 
-    print(line)
     if line_idx == 0:
-        cost               = int(line.split(" ")[4])
-        blueprint.ore_cost = cost
+        cost            = int(line.split(" ")[4])
+        blueprint[0][0] = cost
         
     elif line_idx == 1:
-        cost                = int(line.split(" ")[4])
-        blueprint.clay_cost = cost
+        cost            = int(line.split(" ")[4])
+        blueprint[1][0] = cost
         
     elif line_idx == 2:
         ore_cost                = int(line.split(" ")[4])
         clay_cost               = int(line.split(" ")[7])
-        blueprint.obsidian_cost = [ore_cost, clay_cost]
+        blueprint[2][0] = ore_cost
+        blueprint[2][1] = clay_cost
         
     elif line_idx == 3:
         ore_cost             = int(line.split(" ")[4])
         obsidian_cost        = int(line.split(" ")[7])
-        blueprint.geode_cost = [ore_cost, obsidian_cost]
-    else:
-        print(f"???: {line_idx}")
-    
+        blueprint[3][0] = ore_cost
+        blueprint[3][2] = obsidian_cost
+            
     line_idx += 1
 
+score = 0
+for idx, result in enumerate(blueprint_results):
+    if result:
+        print(result.history)
+        score += (idx + 1) * result.resources[3]
 
-total = 0
-for idx, a in enumerate(geode_count):
-    print(a.geode_count)
-    total += (idx + 1) * a.geode_count
 
-print(f"score: {total}")
+for idx, result in enumerate(blueprint_results):
+    if result:
+        print(result.resources[3])
+        
+
+print(f"score: {score}")
